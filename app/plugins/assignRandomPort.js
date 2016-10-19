2436e7ad03ffe1615b95ba9c552ed81304745833
Elsner Lukas (IT-SWE-MWE-WZU) <lukas.elsner@sbb.ch>
2016-10-19 16:36:16 +0200

    make ports real random

--- app/plugins/assignRandomPort.js
+++ app/plugins/assignRandomPort.js
@@ -3,22 +3,14 @@
 const fs = require('fs'),
     async = require('asyncawait/async'),
     await = require('asyncawait/await'),
-    network = require('network'),
-    portastic = require('portastic');
+    network = require('network');
 
 var assignRandomPortPlugin = async(function (scaler) {
     scaler.hooks.beforeCreate.push(function (config, args) {
         var container = args[1],
-            containerConfig = args[2],
-            runningContainers = await(scaler.getDockerInfo()).Containers;
-
+            containerConfig = args[2];
         if (container.randomPort != undefined && container.randomPort) {
-            var randomPort = await(portastic.find({
-                min: config.minPort + runningContainers,
-                max: config.maxPort,
-                retrieve: 1
-            }));
-
+            var randomPort = Math.floor(Math.random() * (config.maxPort - config.minPort + 1) + config.minPort);
             containerConfig.PortBindings[randomPort + "/tcp"] = [{
                 HostIp: "0.0.0.0",
                 HostPort: randomPort.toString()
@@ -28,21 +20,14 @@ var assignRandomPortPlugin = async(function (scaler) {
         }
 
         if (container.randomPorts != undefined && Array.isArray(container.randomPorts)) {
-            var j = 0;
             for (var i in container.randomPorts) {
-                var randomPort = await(portastic.find({
-                    min: config.minPort + runningContainers + parseInt(i),
-                    max: config.maxPort,
-                    retrieve: 1
-                }));
-
+                var irandomPort = Math.floor(Math.random() * (config.maxPort - config.minPort + 1) + config.minPort);
                 var port = container.randomPorts[i] + "/tcp";
                 containerConfig.PortBindings[port] = [{
                     HostIp: "0.0.0.0",
-                    HostPort: randomPort.toString()
+                    HostPort: irandomPort.toString()
                 }];
-                containerConfig.ExposedPorts[randomPort.toString() + "/tcp"] = {};
-                containerConfig.Env.push("RANDOM_PORT_" + container.randomPorts[i] + "=" + randomPort);
+                containerConfig.Env.push("RANDOM_PORT_" + container.randomPorts[i] + "=" + irandomPort);
             }
         }
     });
