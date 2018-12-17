d817b8fb5bed6fb1275665b8be8f1ad2ce5f0694
Pascal Obrist <pascal.obrist@sbb.ch>
2018-12-17 16:06:09 +0100

    :

--- app/src/plugins/remove-idle-jenkins-slaves.js
+++ app/src/plugins/remove-idle-jenkins-slaves.js
@@ -205,7 +205,7 @@ for (Node node in jenkinsNodes)
             let response = await this._postRequest(this._scriptUrl, {
                 data: "script=" + this.getAllNodesJenkinsScript()
             });
-            const serverList = response.data.trim().split("\n");
+            const serverList = trim(response.data).split("\n");
             if (serverList.length === 0) {
                 throw "Didn't get any server from API";
             }
